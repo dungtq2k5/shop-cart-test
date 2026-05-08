bce094796c5205f69729cf62461a317927765eb6
dungtq2k5 <dungtranquang2005@gmail.com>
2026-05-08 12:26:53 +0700

    test(frontend): update test code to up-to-date within the frontend code

--- client/e2e/cart.e2e.spec.ts
+++ client/e2e/cart.e2e.spec.ts
@@ -184,6 +184,6 @@ test.describe("Cart E2E Tests", () => {
     // Try adding to cart to see error message
     await page.locator("#product-modal-add-to-cart").click();
     const toastMessage = page.getByRole("status");
-    await expect(toastMessage).toContainText("Failed to add item to cart");
+    await expect(toastMessage).toContainText("Insufficient stock");
   });
 });
